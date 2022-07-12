4b66ccc75757898a922a7179a42ef99ad6d59068
helloalicesmith <helloalicesmith@gmail.com>
2022-07-12 18:58:40 +0300

    python: docstring was added

--- decorator.py
+++ decorator.py
@@ -1,19 +1,26 @@
+import functools
+
 def tag_wrapper(tag):
     def close_tag(tag):
         # TODO closing tag
         return tag
 
     def decorator(func):
+        @functools.wraps(func)
         def wrapper(*args, **kwargs):
+            """add html tag"""
             return tag + func(*args, **kwargs) + close_tag(tag)
         return wrapper
 
     return decorator 
 
+@tag_wrapper('<div>')
 @tag_wrapper('<h1>')
 def say(x):
+    """say something"""
     return x
 
 result = say('hello')
 
+print(f'doc: {say.__doc__}, name: {say.__name__}')
 print(result)
